4f6e39e1cd081b41cb3c4b153adc7fa93b4e9e44
Justin Strelka <jstrelka@student.cccs.edu>
2020-06-11 18:25:22 -0600

    moving to linux

--- src/main.c
+++ src/main.c
@@ -6,21 +6,23 @@
 */
 
 #define typedef int bool;
-const string filepath = "SudokuPuzzle.txt";
+const string filename = "SudokuPuzzle.txt";
 int sudokuPuzzle[9][9];
 
 
-void populateArray(string filepath, int *sudokuPuzzle[9][9]);
+void populateArray(string filename, int *sudokuPuzzle[9][9]);
 
 
 int main(){
 
-    populateArray(filepath, sudokuPuzzle);
+    populateArray(filename, sudokuPuzzle);
 
 
 }
 
 
 void populateArray(string filepath, int *sudokuPuzzle[9][9]){
-
+    FILE *fp;
+    fp = fopen(filename, "r");
+    int fscanf(fp)
 }
